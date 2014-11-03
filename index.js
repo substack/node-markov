6d5b8245a491bd58518d0ee35cad364ec6c9d2c8
Ben Robbins <ben.robbins@gmail.com>
2014-11-03 09:03:03 -0600

    add a few comments, change var names

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var _ = require('underscore');
 
 function Markov(minimumWords) {
   this.minimumWords = minimumWords || 1;
-  this.db = {};
+  this.model = {};
 }
   
-//TODO comments
+//update the model using the supplied string
 Markov.prototype.train = function(str) {
   var text = (Buffer.isBuffer(str) ? str.toString() : str)
   var words = this.wordsFromText(text);
@@ -21,7 +21,7 @@ Markov.prototype.train = function(str) {
       next = words[i + 1];
       prev = words[i - 1];
 
-      node = this.addDefaultDbRow(word);
+      node = this.addDefaultModelNode(word);
 
       this.incrementCount(node);
 
@@ -43,18 +43,22 @@ Markov.prototype.train = function(str) {
   }
 };
 
+//compute a node's weight using its count. uses ln(count) to prevent some nodes from being highly favored
 Markov.prototype.computeWeight = function(count) {
   return Math.log(count) + 1;
 };
 
+//break a string into words, and remove punctuation, etc.
 Markov.prototype.wordsFromText = function(text) {
   return _.map(text.toString().split(/\s+/), clean);
 };
 
+//pick a word from the model, favoring words that appear in `text`
 Markov.prototype.search = function(text) {
-  return this.pickWord(this.db, this.wordsFromText(text));
+  return this.pickWord(this.model, this.wordsFromText(text));
 };
   
+//pick a word from `nodes`, optionally favoring words in `words`
 Markov.prototype.pickWord = function(nodes, words) {
   if (words) {
     words = _.intersection(_.keys(nodes), words)
@@ -81,22 +85,25 @@ Markov.prototype.pickWord = function(nodes, words) {
   }, null);
 };
 
+//pick a word to follow `word`
 Markov.prototype.next = function(word) {
-  if (!word || !this.db[word]) {
+  if (!word || !this.model[word]) {
     return undefined;
   }
   
-  return this.pickWord(this.db[word].next);
+  return this.pickWord(this.model[word].next);
 };
 
+//pick a word to precede `word`
 Markov.prototype.prev = function(word) {
-  if (!word || !this.db[word]) {
+  if (!word || !this.model[word]) {
     return undefined;
   }
   
-  return this.pickWord(this.db[word].prev);
+  return this.pickWord(this.model[word].prev);
 };
 
+//construct a sentence starting from `word`, with at most `limit` words
 Markov.prototype.fill = function(word, limit) {
   var response = [word];
 
@@ -141,29 +148,34 @@ Markov.prototype.fill = function(word, limit) {
   return response.join(' ');
 };
 
+//construct a response to `text` with at most `limit` words
 Markov.prototype.respond = function(text, limit) {
   limit = limit || 25;
   return this.fill(this.search(text), limit);
 };
 
+//export the current model and settings as a JSON string
 Markov.prototype.export = function() {
-  return JSON.stringify(_.pick(this, ['db', 'minimumWords']));
+  return JSON.stringify(_.pick(this, ['model', 'minimumWords']));
 }
 
+//import model and settings from a JSON string
 Markov.prototype.import = function(json) {
   var data = JSON.parse(json);
-  _.extend(this, _.pick(data, ['db', 'minimumWords']));
+  _.extend(this, _.pick(data, ['model', 'minimumWords']));
 };
 
-Markov.prototype.addDefaultDbRow = function(word) {
-  if (!_.isObject(this.db[word])) {
-    this.db[word] = {};
+//create a default node in the model
+Markov.prototype.addDefaultModelNode = function(word) {
+  if (!_.isObject(this.model[word])) {
+    this.model[word] = {};
   }
-  _.defaults(this.db[word], defaultDbRow());
+  _.defaults(this.model[word], defaultModelNode());
 
-  return this.db[word];
+  return this.model[word];
 };
 
+//increment a node's count and re-compute its weight
 Markov.prototype.incrementCount = function(obj) {
   obj = _.isObject(obj) ? obj : {count: 0, weight: 0};
 
@@ -173,6 +185,7 @@ Markov.prototype.incrementCount = function(obj) {
   return obj;
 };
 
+//clean a string
 function clean(s) {
   return s
     .toLowerCase()
@@ -181,7 +194,8 @@ function clean(s) {
     .replace(/_$/, '');
 }
 
-function defaultDbRow() {
+//default node
+function defaultModelNode() {
   return {
     count: 0,
     next: {},
@@ -189,6 +203,7 @@ function defaultDbRow() {
   };
 }
 
+//map a list to an object, using list values as keys
 function mapObject(list, fn) {
   var ret = {};
 
